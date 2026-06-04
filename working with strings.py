26721341aab3a3d9a90f80497e4ce8d25866428d
Vivek Anand <email.vivekanand28@gmail.com>
2026-06-04 14:41:30 +0400

    Update working with strings.py

--- working with strings.py
+++ working with strings.py
@@ -1,7 +1,7 @@
 #WORKING WITH STRINGS
 
 print("Rochester Institute of Technology, Dubai")
-print("Rochester Institute of Technology\nDubai")                            #NEWLINE CHARACTER - \n
+print("Rochester Institute of Technology\nDubai")                                #NEWLINE CHARACTER - \n
 
 college = "Rochester Institute of Technology, Dubai"
 print("I study in " +college)
@@ -12,4 +12,4 @@ print(college.upper().isupper())                                                
 print(len(college))                                                              #DETERMINE THE LENGTH OF THE STRING
 print(college[28])                                                               #PRINTS THE nth CHARACTER OF STRING - n is the index
 print(college.index('T'))                                                        #PRINTS THE INDEX OF THE GIVEN CHARACTER
-print(college.replace('Rochester Institute of Technology', 'RIT'))      #REPLACES WORDS/LETTERS IN A STRING
+print(college.replace('Rochester Institute of Technology', 'RIT'))               #REPLACES WORDS/LETTERS IN A STRING
